6b25a7f36864ac02f98ebf7308a0ba3853f14330
Deepak Sharma <deepak.sharma@decisions.no>
2023-08-17 06:25:32 +0530

    main file which will host all steps for MMKG

---
+++ main.py
@@ -0,0 +1,12 @@
+from text_extraction import scrape_article
+
+#output = scrape_article('http://www.dailymail.co.uk/news/article-3197220/CHRISTOPHER-BOOKER-Lunacy-biggest-white-elephant-Britain.html')
+output = scrape_article('https://www.nytimes.com/2023/08/15/us/politics/trump-cases-counts-charges-strengths.html')
+
+print(output)
+
+
+
+
+
+
