507f95322359bd4fe060f5e76f3196b5185a0ee4
Deepak Sharma <deepak.sharma@decisions.no>
2023-08-20 08:49:49 +0530

    save each step output in output json files

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from text_extraction import scrape_article
+from text_extraction import ArticleEncoder, scrape_article
+from extract_triples import get_openie_triples
+import json
 
-#output = scrape_article('http://www.dailymail.co.uk/news/article-3197220/CHRISTOPHER-BOOKER-Lunacy-biggest-white-elephant-Britain.html')
-output = scrape_article('https://www.nytimes.com/2023/08/15/us/politics/trump-cases-counts-charges-strengths.html')
 
-print(output)
+output = scrape_article('http://www.dailymail.co.uk/news/article-3197220/CHRISTOPHER-BOOKER-Lunacy-biggest-white-elephant-Britain.html')
+
+with open('output_step1_text_extraction.json', 'w') as f:
+    json.dump(output, f, cls=ArticleEncoder)
+
+
+triples = get_openie_triples(output.textContent)
+with open('output_step2_triples_extraction.json', 'w') as f:
+    json.dump(triples, f)
+
+
 
 
 
